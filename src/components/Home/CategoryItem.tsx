bee6c936581a0418c59f0eba812551dd8157bd47
amaechichisom <amaechilegend@gmail.com>
2025-05-22 07:28:52 +0200

    fixing responsiveness

--- src/components/Home/CategoryItem.tsx
+++ src/components/Home/CategoryItem.tsx
@@ -1,18 +1,19 @@
 import { SaleIcon } from '@/assets'
 
-function CategoryItem({ src, title}: {src?: string, title?: string}) {
+function CategoryItem({ src, title }: { src?: string, title?: string }) {
   return (
-    <div className='flex flex-col items-center justify-center gap-2 p-[3px] pb-3 max-w-40 max-h-48 mx-auto lg:mx-0'>
-      <div className="relative">
-          <img src={src ?? SaleIcon} 
-          alt="Property" 
-          className="w-full object-cover"/>
+    <div className="flex flex-col items-center justify-center gap-2 p-2 pb-3 w-full max-w-[160px] min-w-[100px] mx-auto lg:mx-0">
+      <div className="relative w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28">
+        <img
+          src={src ?? SaleIcon}
+          alt="Property"
+          className="w-full h-full object-cover rounded-xl"
+        />
       </div>
-
-      <p className='font-semibold text-sm text-neutral-500 px-2 truncate'>{title || "Not set"}</p>
+      <p className="font-semibold text-xs sm:text-sm text-neutral-500 px-2 truncate text-center">
+        {title || "Not set"}
+      </p>
     </div>
-
-
   )
 }
 
